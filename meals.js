75f2af30a2e18de9a6ff4100642682f893e1365d
Tuanheredontscared <77202166+Tuanheredontscared@users.noreply.github.com>
2022-04-18 20:54:08 +0700

    Add files via upload

---
+++ meals.js
@@ -0,0 +1,97 @@
+const searchBtn = document.getElementById("search-btn");
+const mealsList = document.getElementById("meals");
+const mealDetailsContent = document.querySelector('.meal-details-content');
+const recipeCloseBtn = document.getElementById('recipe-close-btn');
+const search_btn = document.getElementById('search-input')
+const searchInputTxt = document.getElementById("search-input")
+// const input = document.getElementById('search-input')
+mealsList.addEventListener('click', getMealRecipe);
+
+function recipe_btn() {
+  mealDetailsContent.parentElement.classList.remove('showRecipe');
+}
+searchBtn.addEventListener('click', findMeals)
+
+
+
+async function fetchMealsApi() {
+  let searchInputTxt = document.getElementById("search-input").value
+  const meals = await fetch(
+    `https://www.themealdb.com/api/json/v1/1/filter.php?i=${searchInputTxt}`
+  );
+  const body = await meals.json();
+  console.log(body);
+  return body ?.meals;
+
+}
+
+// find meals
+const mealsArray = ['spray', 'limit', 'elite', 'exuberant', 'destruction', 'present'];
+
+function findMeals() {
+  let searchInputTxt = document.getElementById("search-input").value;
+  const result = mealsArray.filter(function (a) {
+    return a === searchInputTxt;
+  })
+
+}
+
+// const words = ['spray', 'limit', 'elite', 'exuberant', 'destruction', 'present'];
+
+// const result = words.filter(function (word) { return word.length > 6 });
+
+// console.log(result);
+
+function mealsPrint(meals) {
+  let mealsText = "";
+  Array.from(meals).forEach(function (meals) {
+    mealsText +=
+      `<div class = "meal-item" data-id = "${meals.idMeal}">
+    <div class = "meal-img">
+        <img src = ${meals.strMealThumb} alt = "food">
+    </div>
+    <div class = "meal-name">
+        <h3>${meals.strMeal}</h3>
+        <a href = "#" class = "recipe-btn">Get Recipe</a>
+    </div>
+</div>`;
+  });
+  mealsList.innerHTML = mealsText;
+}
+
+fetchMealsApi().then(function (meals) {
+  mealsPrint(meals)
+});
+
+
+
+
+// get recipe
+async function getMealRecipe(event) {
+  event.preventDefault();
+  let mealItem = event.target.parentElement.parentElement
+  const getRecipe = await fetch(`https://www.themealdb.com/api/json/v1/1/lookup.php?i=${mealItem.dataset.id}`)
+  const js = await getRecipe.json()
+  createRecipe(js.meals)
+  console.log(js)
+}
+
+
+function createRecipe(data) {
+  let recipeText = `
+      <h2 class = "recipe-title">${data.strMeal}</h2>
+      <p class = "recipe-category">${data.strCategory}</p>
+      <div class = "recipe-instruct">
+          <h3>Instructions:</h3>
+          <p>${data.strInstructions}</p>
+      </div>
+      <div class = "recipe-meal-img">
+          <img src = "${data.strMealThumb}" alt = "">
+      </div>
+      <div class = "recipe-link">
+          <a href = "${data.strYoutube}" target = "_blank">Watch Video</a>
+      </div>
+  `;
+  mealDetailsContent.innerHTML = recipeText;
+  mealDetailsContent.parentElement.classList.add('showRecipe');
+}
